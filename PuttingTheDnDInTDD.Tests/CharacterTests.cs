af92c17cdceb5c1713422d075c2fff34d2bf2ba1
Jeffroe <jturner@sparkoverflow.com>
2014-04-28 10:39:47 -0400

    first commit

---
+++ PuttingTheDnDInTDD.Tests/CharacterTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PuttingTheDnDInTDD.Tests
+{
+    [TestFixture]
+    public class CharacterTests
+    {
+        [Test]
+        public void CanGetAndSetCharacterName()
+        {
+            var c = new Character(); 
+            c.Name = "Some Name" ;
+            Assert.AreEqual("Some Name", c.Name);
+        }
+
+        [Test]
+        public void CanGetAndSetKnownAlignmentValues( [Values("Good", "Evil", "Neutral")] string input)
+        {
+            var c = new Character();
+            c.Alignment = input;
+            Assert.AreEqual(input, c.Alignment);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void UnknownAlignmentValueThrowsException()
+        {
+            var c = new Character();
+            c.Alignment = "Awesome";
+        }
+
+        [Test]
+        public void CharacterArmorDefaultsTo10()
+        {
+            var c = new Character();
+            Assert.AreEqual(10, c.Armor);
+        }
+
+        [Test]
+        public void CharacterHitPointsDefaultsTo5()
+        {
+            var c = new Character();
+            Assert.AreEqual(5, c.HitPoints);
+        }
+
+        [Test]
+        public void CharacterCantHitWithRollOf9()
+        {
+            var roll = 9;
+            var c = new Character();
+            c.Attack(roll);
+            Assert.AreEqual(10, c.Armor);
+        }
+        
+        [Test]
+        public void CharacterCanHitWithRollOf10()
+        {
+            var roll = 10;
+            var c = new Character();
+            c.Attack(roll);
+            Assert.AreEqual(5, c.Armor);
+        }
+
+        [Test]
+        public void CharacterCanHitWithRollOf11()
+        {
+            var roll = 11;
+            var c = new Character();
+            c.Attack(roll);
+            Assert.AreEqual(5, c.Armor);
+        }
+    }
+}
---
+++ PuttingTheDnDInTDD/Character.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace PuttingTheDnDInTDD
+{
+    public class Character
+    {
+        private string _alignment;
+
+        public string Name { get; set; }
+        public string Alignment
+        {
+            get { return _alignment; }
+            set
+            {
+                var validAlignments = new List<string>() { "Good", "Evil", "Neutral" };
+                if (validAlignments.Contains(value))
+                {
+                    _alignment = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+        public int Armor { get; set; }
+        public int HitPoints { get; set; }
+
+        public Character()
+        {
+            Armor = 10;
+            HitPoints = 5;
+        }
+
+        public void Attack(int roll)
+        {
+            if (roll >= 10)
+            {
+                Armor = 5;
+            }
+        }
+    }
+}
